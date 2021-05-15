7dc8a7505c4eb252f8cd0829d3b2088fc6b4741c
pilotich <72825010+pilotich@users.noreply.github.com>
2021-05-15 17:36:43 +0300

    writing a structure for service page

--- museum-react-app/src/App.js
+++ museum-react-app/src/App.js
@@ -1,24 +1,90 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import EventScheduler from "./components/EventScheduler"
+import Gallery from "./components/Gallery";
+import VisitExhibition from "./components/VisitExhibition";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+  useLocation,
+} from "react-router-dom";
+import moment from 'moment';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div className="App">
+          <header>
+            <div className="logo">
+              <a><Link to="/"><img src={logo}></img></Link></a>
+            </div>
+            <ul>
+              <li>
+                <Link to="/games"></Link>
+              </li>
+              <li>
+                <Link to="/events"></Link>
+              </li>
+              <li>
+                <Link to="/shop"></Link>
+              </li>
+            </ul>
+          </header>
+
+          <section>
+            <Switch>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/previews">
+                <Games />
+              </Route>
+              <Route path="/events">
+                <Events />
+              </Route>
+              <Route path="/excursions">
+                <Excursions />
+              </Route>
+              <Route path="*">
+                <NoMatch />
+              </Route>
+            </Switch>
+
+          </section>
+
+          <footer>
+            <div className="footerText">
+
+            </div>
+          </footer>
+        </div>
+      </Router>
+  );
+}
+
+function NoMatch() {
+  let location = useLocation();
+
+  return (
+      <div>
+        <h3>
+          No match for <code>{location.pathname}</code>
+        </h3>
+      </div>
+  );
+}
+
+function About() {
+  let location = useLocation();
+
+  return (
+      <div>
+        <h2>About us</h2>
+      </div>
   );
 }
 
