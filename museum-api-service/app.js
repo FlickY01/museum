e9bca7a527682a4e27e8fb8e51f044b1cc796880
FlickY01 <Vladislav.sbyr@gmail.com>
2021-05-15 16:14:36 +0300

    app.js & package.json

--- museum-api-service/app.js
+++ museum-api-service/app.js
@@ -17,7 +17,13 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
   var port = process.env.PORT || 10010;
   app.listen(port);
 
-  if (swaggerExpress.runner.swagger.paths['/hello']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
+  if (swaggerExpress.runner.swagger.paths['/events']) {
+    console.log('try this:\ncurl http://127.0.0.1:' + port + '/api/v1/events');
+  }
+  if (swaggerExpress.runner.swagger.paths['/excursions']) {
+    console.log('try this:\ncurl http://127.0.0.1:' + port + '/api/v1/excursions');
+  }
+  if (swaggerExpress.runner.swagger.paths['/previews']) {
+    console.log('try this:\ncurl http://127.0.0.1:' + port + '/api/v1/previews');
   }
 });
