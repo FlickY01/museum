cae1c960c2c98d106e6c37ec79883ef4a9d6e207
FlickY01 <Vladislav.sbyr@gmail.com>
2021-05-15 16:09:58 +0300

    Swagger edit!

--- museum-api-service/api/controllers/event-controller.js
+++ museum-api-service/api/controllers/event-controller.js
@@ -11,7 +11,7 @@
   It is a good idea to list the modules that your application depends on in the package.json in the project root
  */
 var util = require('util');
-
+var faker = require('faker');
 /*
  Once you 'require' a module you can reference the things that it exports.  These are defined in module.exports.
 
@@ -25,7 +25,7 @@ var util = require('util');
   we specify that in the exports of this module that 'hello' maps to the function named 'hello'
  */
 module.exports = {
-  hello: hello
+  events: getEvents,
 };
 
 /*
@@ -34,11 +34,43 @@ module.exports = {
   Param 1: a handle to the request object
   Param 2: a handle to the response object
  */
-function hello(req, res) {
+function getEvents(req, res) {
+  function todayDate() {
+    var today = new Date();
+    var dd = today.getDate();
+
+    var mm = today.getMonth() + 1;
+    var yyyy = today.getFullYear();
+    if (dd < 10) {
+      dd = '0' + dd;
+    }
+
+    if (mm < 10) {
+      mm = '0' + mm;
+    }
+    var d = new Date(today),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2)
+      month = '0' + month;
+    if (day.length < 2)
+      day = '0' + day;
+
+    return [year, month, day].join('-');
+  }
   // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
-  var name = req.swagger.params.name.value || 'stranger';
-  var hello = util.format('Hello, %s!', name);
+  var date = req.swagger.params.date.value || todayDate();
+
 
   // this sends back a JSON response which is a single string
-  res.json(hello);
+  res.json([{
+    name: faker.name.firstName(),
+    image: faker.image.imageUrl(),
+    date: date,
+    location: "Minsk, Prime Hall" ,
+    price: faker.random.number(),
+  },
+  ]);
 }
